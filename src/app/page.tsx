0a21fb8efd1d486c34da85461e86a43b4638fe70
seongkyu-lim <96x60812@gmail.com>
2025-05-26 20:59:44 +0900

    feat: main page css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,45 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="container mx-auto px-4 py-12 text-center">
-      <h1 className="text-4xl font-bold mb-4">📚 Power Clean</h1>
-      <p className="text-lg mb-8 text-gray-600">
+    // <div className="container mx-auto px-4 py-12 text-center">
+    //   <p className="text-lg mb-8 text-gray-600">
+    //     Share book information and engage in discussions!
+    //   </p>
+    //   <div className="flex justify-center gap-4">
+    //     <button
+    //       className="bg-blue-600 text-white px-6 py-3 rounded-2xl shadow hover:bg-blue-700 transition duration-200"
+    //       onClick={() => router.push("/post/create")}
+    //     >
+    //       Add Post ➕
+    //     </button>
+    //     {/* <button
+    //       className="bg-green-500 text-white px-6 py-3 rounded-2xl shadow hover:bg-green-600 transition duration-200"
+    //       onClick={() => router.push("/post")}
+    //     >
+    //       View Posts 📄
+    //     </button> */}
+    //   </div>
+    // </div>
+    <div className="min-h-screen w-full bg-[#ede4cf] text-[#4b3b2a] font-serif">
+      {/* <div className="container mx-auto px-4 py-12 text-center"> */}
+      <p className="text-lg mb-8 text-[#5c4033]">
         Share book information and engage in discussions!
       </p>
       <div className="flex justify-center gap-4">
         <button
-          className="bg-blue-600 text-white px-6 py-3 rounded-2xl shadow hover:bg-blue-700 transition duration-200"
+          className="bg-[#ede4cf] text-white px-6 py-3 rounded-2xl shadow hover:bg-[#6a563f] transition duration-200"
           onClick={() => router.push("/post/create")}
         >
           Add Post ➕
         </button>
-        <button
-          className="bg-green-500 text-white px-6 py-3 rounded-2xl shadow hover:bg-green-600 transition duration-200"
+        {/* <button
+          className="bg-[#557153] text-white px-6 py-3 rounded-2xl shadow hover:bg-[#445f43] transition duration-200"
           onClick={() => router.push("/post")}
         >
-          View Posts 📄
-        </button>
+          View Posts 
+        </button> */}
       </div>
+      {/* </div> */}
     </div>
   );
 }
