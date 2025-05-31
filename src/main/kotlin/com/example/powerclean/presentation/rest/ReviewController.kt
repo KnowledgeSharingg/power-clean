7d711d2f5d3c5f0a174c0ac7709b104c24ad6a39
seongkyulim <96x60812@gmail.com>
2025-05-31 22:32:06 +0900

    feat: ReviewController

---
+++ src/main/kotlin/com/example/powerclean/presentation/rest/ReviewController.kt
@@ -0,0 +1,51 @@
+package com.example.powerclean.presentation.rest
+
+
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+
+
+@RestController
+@RequestMapping("/review")
+class ReviewController(private val reviewService: ReviewService) {
+
+    @Operation(summary = "리뷰 생성 API", description = "리뷰를 생성합니다.")
+    @PostMapping
+    fun createReview(
+        @RequestBody request: CreateReviewReqDto,
+    ): CreateReviewResDto = reviewService.createReview(request)
+
+    @Operation(summary = "리뷰 상세 조회 API", description = "리뷰의 상세 정보를 조회합니다.")
+    @GetMapping("/{reviewId}")
+    fun getReviewDetail(
+        @PathVariable reviewId: UUID,
+    ): GetReviewDetailResDto = reviewService.getReviewDetail(reviewId)
+
+    @Operation(summary = "리뷰 리스트 조회 API", description = "리뷰 리스트를 조회합니다.")
+    @GetMapping("/list")
+    fun getReviewList(
+        @RequestParam page: Int,
+        @RequestParam size: Int,
+    ): GetReviewListResDto = reviewService.getReviewList(page, size)
+
+    @Operation(summary = "리뷰 수정 API", description = "리뷰를 수정합니다.")
+    @PatchMapping
+    fun updateReview(
+        @RequestBody request: UpdateReviewReqDto,
+    ): String = reviewService.updateReview(request)
+    @Operation(summary = "리뷰 삭제 API", description = "리뷰를 삭제합니다.")
+    
+    @DeleteMapping("/{reviewId}")
+    fun deleteReview(
+        @PathVariable reviewId: UUID,
+    ): String = reviewService.deleteReview(reviewId)
+}
