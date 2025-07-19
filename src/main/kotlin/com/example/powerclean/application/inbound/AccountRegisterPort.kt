54c8eddd3c3fa5610449fd4ccb8f47c184005b8f
seongkyulim <96x60812@gmail.com>
2025-07-19 22:15:28 +0900

    feat: add port for register account

---
+++ src/main/kotlin/com/example/powerclean/application/inbound/AccountRegisterPort.kt
@@ -0,0 +1,11 @@
+package com.example.powerclean.application.inbound
+
+import com.example.powerclean.presentation.dto.RegisterAccountReqDto
+import com.example.powerclean.presentation.dto.RegisterAccountResDto
+
+interface AccountRegisterPort {
+    /*
+     * 계정 등록.
+     */
+    fun registerAccount(requestDto: RegisterAccountReqDto): RegisterAccountResDto
+}
