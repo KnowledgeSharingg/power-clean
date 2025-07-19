fde02b6783546975f363443408ebd07126a7c91a
seongkyulim <96x60812@gmail.com>
2025-07-19 22:15:46 +0900

    feat: 이메일 인증 인가 API

--- src/main/kotlin/com/example/powerclean/presentation/rest/AccountController.kt
+++ src/main/kotlin/com/example/powerclean/presentation/rest/AccountController.kt
@@ -1,8 +1,35 @@
 package com.example.powerclean.presentation.rest
 
+import com.example.powerclean.application.service.AccountService
+import com.example.powerclean.presentation.dto.RegisterAccountReqDto
+import com.example.powerclean.presentation.dto.RegisterAccountResDto
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+// 가설: 인바운드 상황에서 port는 service 구현체들의 명세서로 사용되고, adaptor에서는 구현체를 바로 사용해서 의존성을 내부로 향하게 유지한다.
 @RestController
 @RequestMapping("/account")
-class AccountController
+class AccountController(private val accountService: AccountService) {
+    @Operation(summary = "회원 가입 API", description = "새로운 계정을 등록합니다.")
+    @PostMapping("/register")
+    fun registerAccount(
+        @RequestBody requestDto: RegisterAccountReqDto,
+    ): RegisterAccountResDto {
+        return this.accountService.registerAccount(requestDto)
+    }
+
+    // @Operation(summary = "로그인 API", description = "사용자 인증을 수행합니다.")
+    // @PostMapping("/login")
+    // fun login(@RequestBody requestDto: LoginReqDto): LoginResDto {
+    //     return this.accountService.login(requestDto)
+    // }
+
+    // @Operation(summary = "사용자 정보 조회 API", description = "사용자의 정보를 조회합니다.")
+    // @GetMapping("/info")
+    // fun getUserInfo(): GetUserInfoResDto {
+    //     return this.accountService.getUserInfo()
+    // }
+}
