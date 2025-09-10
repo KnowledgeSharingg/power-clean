b5954565ba2c00f34bf5416350bd9e5041ae9730
seongkyu-lim <96x60812@gmail.com>
2025-09-10 16:56:13 +0900

    feat: AccountAuthenticationUseCase 구현체랑 매핑 config ㅅㅓㄹㅈㅓㅇ ㅋㅗㄷㅡ ㅃㅒㄱㅣ.

--- src/main/kotlin/com/example/powerclean/config/ServiceConfig.kt
+++ src/main/kotlin/com/example/powerclean/config/ServiceConfig.kt
@@ -1,13 +1,6 @@
 package com.example.powerclean.config
 
-import com.example.powerclean.application.inbound.AccountAuthenticateUseCase
-import com.example.powerclean.application.service.AuthenticationService
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class ServiceConfig {
-    @Bean("accountAuthenticationUseCase")
-    fun authenticationService(authenticationService: AuthenticationService): AccountAuthenticateUseCase =
-        authenticationService
-}
+class ServiceConfig
