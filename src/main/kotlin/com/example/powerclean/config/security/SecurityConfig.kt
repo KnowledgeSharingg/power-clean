7bee001cb25e86f41799e07deb54dd41b8b97e43
seongkyulim <96x60812@gmail.com>
2026-02-22 08:53:54 +0000

    feat: add localhost:3001 to CORS allowed origins

--- src/main/kotlin/com/example/powerclean/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/powerclean/config/security/SecurityConfig.kt
@@ -110,6 +110,8 @@ class SecurityConfig {
             listOf(
                 "http://127.0.0.1:3000",
                 "http://localhost:3000",
+                "http://127.0.0.1:3001",
+                "http://localhost:3001",
                 "https://power-clean-client.vercel.app",
                 "https://barely-settled-iguana.ngrok-free.app",
             )
